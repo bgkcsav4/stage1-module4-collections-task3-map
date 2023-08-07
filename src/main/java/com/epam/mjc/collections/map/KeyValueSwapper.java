57cbbf331c43c225f80452978921620fc33b29eb
Latsa <bgkcsavvy@gmail.com>
2023-08-07 11:06:32 +0200

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -2,7 +2,18 @@ package com.epam.mjc.collections.map;
 
 import java.util.Map;
 
+import java.util.HashMap;
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            swappedMap.put(value, key);
+        }
+
+        return swappedMap;
     }
+
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("\\W+");
+
+        for (String word : words) {
+            wordRepetitionMap.put(word, wordRepetitionMap.getOrDefault(word, 0) + 1);
+        }
+
+        return wordRepetitionMap;
     }
 }
